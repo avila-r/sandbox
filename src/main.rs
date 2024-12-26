feb88c980dd3b2c77558b010040d608c07534f59
r. ávila <avila.dev@outlook.com>
2024-12-25 23:46:46 -0300

    feat: add binary search

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,30 @@ fn main() {
         "Reverse Words in a String III: {}",
         reverse(String::from("Let's take LeetCode contest"))
     }
+
+    println! {
+        "Binary search: {}",
+        match binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 3) {
+            Some(i) => format!("Target was found in index {i}"),
+            None => "Target not found".to_string()
+        }
+    }
+}
+
+fn binary_search(numbers: Vec<i32>, target: i32) -> Option<usize> {
+    let (mut left, mut right) = (0, numbers.len());
+
+    while left < right {
+        let mid: usize = left + (right - left) / 2;
+
+        match numbers[mid] {
+            v if v.eq(&target) => return Some(mid),
+            v if v < target => left = mid + 1,
+            _ => right = mid,
+        }
+    }
+
+    None
 }
 
 fn reverse(s: String) -> String {
