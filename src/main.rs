f830d046033f5f580d5dfcd079f1f467d8d63e6b
r. ávila <avila.dev@outlook.com>
2024-12-26 00:27:48 -0300

    feat: add sliding window

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,66 @@ fn main() {
             None => "Target not found".to_string()
         }
     }
+
+    println! {
+        "Generic Sliding Window: {}",
+        match sliding_window("bcbbbcba".chars().collect()) {
+            Some(i) => format!("Maximum substring length with two occurrences: {i}"),
+            _ => "?".to_string(),
+        }
+    }
+
+    println! {
+        "Maximum Length Substring With Two Occurrences: {}",
+        match max_substring("bcbbbcba".to_string(), 2) {
+            Some(i) => format!("Maximum substring length with two occurrences: {i}"),
+            _ => "?".to_string(),
+        },
+    }
+}
+
+fn max_substring(s: String, max_occurrences: i32) -> Option<usize> {
+    let (mut left, mut right) = (0, 0);
+    let (chars, mut frequencies, mut max) = (s.chars().collect::<Vec<char>>(), std::collections::HashMap::new(), 0);
+
+    while right < chars.len() {
+        frequencies
+            .entry(chars[right])
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+
+        while frequencies.get(&chars[right]).unwrap_or(&0) > &max_occurrences {
+            frequencies.entry(chars[left]).and_modify(|v| *v -= 1);
+            left += 1;
+        }
+
+        max = max.max(right - left + 1);
+        right += 1;
+    }
+
+    Some(max)
+}
+
+fn sliding_window(chars: Vec<char>) -> Option<i32> {
+    let (mut left, mut right) = (0, 0);
+    let (mut frequencies, mut max) = (std::collections::HashMap::new(), 0);
+
+    while right < chars.len() {
+        frequencies
+            .entry(chars[right])
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+
+        while frequencies.get(&chars[right]).unwrap_or(&0) > &2 {
+            frequencies.entry(chars[left]).and_modify(|v| *v -= 1);
+            left += 1;
+        }
+
+        max = max.max(right - left + 1);
+        right += 1;
+    }
+
+    Some(max as i32)
 }
 
 fn binary_search(numbers: Vec<i32>, target: i32) -> Option<usize> {
