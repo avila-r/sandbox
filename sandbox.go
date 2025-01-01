c3e8169df0833816dc8ff2f0b2ce97bb8a1f6ad2
r. ávila <avila.dev@outlook.com>
2025-01-01 11:58:57 -0300

    feat: add recursive bs

--- sandbox.go
+++ sandbox.go
@@ -17,3 +17,19 @@ func BinarySearch(nums []int, target int) int {
 
 	return -1
 }
+
+func RecursiveBinarySearch(nums []int, target, low, high int) int {
+	if low <= high {
+		mid := (low + high) / 2
+		switch v := nums[mid]; {
+		case v == target:
+			return mid
+		case v < target:
+			return RecursiveBinarySearch(nums, target, mid+1, high)
+		default:
+			return RecursiveBinarySearch(nums, target, low, mid-1)
+		}
+	}
+
+	return -1
+}
